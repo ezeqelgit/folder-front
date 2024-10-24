b84fae9e8c638c30c3094a56b43b67e0ddc84b8c
ezeQel <ezeqelic@icloud.com>
2024-10-24 17:48:57 +0700

    in process / Hacking

--- src/components/GameHacking/Hacking.vue
+++ src/components/GameHacking/Hacking.vue
@@ -15,10 +15,68 @@
           <div class="buffer">
             <span>БУФЕР</span>
             <div class="table">
-              <div class="wrapperTable"></div>
+              <div class="wrapperTable">
+                <div class="timer">
+                  <div class="wrapperTimer">
+                    <div class="title">
+                      <span>ВРЕМЯ ДЕЙСТВИЯ УЯЗВИМОСТИ</span>
+                    </div>
+                    <div class="count">
+                      <span>14:04</span>
+                    </div>
+                  </div>
+                  <div class="separator"></div>
+                </div>
+                <div class="result">
+                  <div class="block">
+                    <div class="cell">
+                      <!-- <div class="numberMatrix">
+                        <span>55</span>
+                      </div>
+                      <div class="separator"></div> -->
+                    </div>
+                    <div class="cell">
+                      <!-- <div class="numberMatrix">
+                        <span>55</span>
+                      </div>
+                      <div class="separator"></div> -->
+                    </div>
+                    <div class="cell">
+                      <!-- <div class="numberMatrix">
+                        <span>55</span>
+                      </div>
+                      <div class="separator"></div> -->
+                    </div>
+                    <div class="cell">
+                      <!-- <div class="numberMatrix">
+                        <span>55</span>
+                      </div>
+                      <div class="separator"></div> -->
+                    </div>
+                    <div class="cell">
+                      <!-- <div class="numberMatrix">
+                        <span>55</span>
+                      </div>
+                      <div class="separator"></div> -->
+                    </div>
+                    <div class="cell">
+                      <!-- <div class="numberMatrix">
+                        <span>55</span>
+                      </div>
+                      <div class="separator"></div> -->
+                    </div>
+                  </div>
+                  <div class="description">
+                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut </span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div class="matrix"></div>
+          <div class="matrix">
+            <div class="blockMatrix"></div>
+            <div class="consistency"></div>
+          </div>
         </div>
       </div>
     </div>
